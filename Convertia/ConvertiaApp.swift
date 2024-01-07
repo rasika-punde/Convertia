687f791f43a7260e549554ffe5da23eb9698b67d
Rasika Punde <rasikapunde@gmail.com>
2024-01-07 12:28:42 -0500

    Initial Commit

---
+++ Convertia/ConvertiaApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConvertiaApp.swift
+//  Convertia
+//
+//  Created by Pratik Mistry on 1/7/24.
+//
+
+import SwiftUI
+
+@main
+struct ConvertiaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
